7db8a7551d66e65566841807a1d3a0db839bf335
AlexQuezada24 <jorge.quezadatrelles@cesunbc.edu.mx>
2025-10-20 08:27:18 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clase-20-oct',
+  webDir: 'www'
+};
+
+export default config;
