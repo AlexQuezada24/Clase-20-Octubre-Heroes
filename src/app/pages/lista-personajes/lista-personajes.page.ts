cb5ce09dd8cee485f3f710312e1d5da3ca6cd1c4
AlexQuezada24 <jorge.quezadatrelles@cesunbc.edu.mx>
2025-10-21 14:58:06 -0700

    Commit 2 clase 21 Octubre

--- src/app/pages/lista-personajes/lista-personajes.page.ts
+++ src/app/pages/lista-personajes/lista-personajes.page.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
 import { Personaje } from 'src/app/interfaces/personaje';
 import { PersonajeService } from 'src/app/services/personaje';
+import { IonContent, IonicModule, ViewWillEnter, LoadingController} from '@ionic/angular'; 
+import { RouterModule } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
+import { finalize } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-lista-personajes',
@@ -11,20 +13,86 @@ import { PersonajeService } from 'src/app/services/personaje';
   styleUrls: ['./lista-personajes.page.scss'],
   standalone: true,
   imports: [
-    CommonModule,
-    IonicModule,
-    RouterModule
-  ],
+    IonicModule,    
+    RouterModule,
+    CommonModule
+    
+  ]
 })
-export class ListaPersonajesPage implements OnInit {
+export class ListaPersonajesPage implements ViewWillEnter {
+  @ViewChild(IonContent) content: IonContent | undefined;
 
-  personajes: Personaje[] = [];
+  allPersonajes: Personaje[] = [];
+  personajesMostrados: Personaje[] = [];
 
-  constructor(private personajeService: PersonajeService) {}
+  paginaActual: number = 0;
+  personajesPorPagina: number = 10;
+  totalPaginas: number = 0;
+  inicioIndex: number = 0;
 
-  ngOnInit() {
-    this.personajeService.getPersonajes().subscribe((data: Personaje[]) => {
-      this.personajes = data;
+  private loading: HTMLIonLoadingElement | null = null;
+
+  constructor(
+    private personajeService: PersonajeService,
+    private loadingCtrl: LoadingController
+  ) { }
+
+  ionViewWillEnter() {
+    this.paginaActual = 0;
+    this.cargarDatosConLoading();
+  }
+
+  async cargarDatosConLoading() {
+    await this.presentLoading();
+
+    this.personajeService.getPersonajes().pipe(
+      finalize(() => {
+        this.hideLoading();
+      })
+    ).subscribe(data => {
+        this.allPersonajes = data;
+        this.totalPaginas = Math.ceil(this.allPersonajes.length / this.personajesPorPagina);
+        this.cargarPersonajes();
+      }, error => {
+        console.error('Error al cargar personajes:', error);
     });
   }
+
+  cargarPersonajes() {
+    this.inicioIndex = this.paginaActual * this.personajesPorPagina;
+    const finIndex = this.inicioIndex + this.personajesPorPagina;
+    this.personajesMostrados = this.allPersonajes.slice(this.inicioIndex, finIndex);
+    this.content?.scrollToTop(500);
+  }
+
+  siguientePagina() {
+    if ((this.paginaActual + 1) < this.totalPaginas) {
+      this.paginaActual++;
+      this.cargarPersonajes();
+    }
+  }
+  
+  isFirstPage(): boolean {
+    return this.paginaActual === 0;
+  }
+
+  isLastPage(): boolean {
+    return (this.paginaActual + 1) === this.totalPaginas;
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Cargando héroes...',
+      spinner: 'crescent'
+    });
+    await this.loading.present();
+  }
+
+  hideLoading() {
+    if (this.loading) {
+      this.loading.dismiss();
+      this.loading = null;
+    }
+  }
+
 }
